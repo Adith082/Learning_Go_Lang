babb28f3b4df764c16acf9a95f695359ceca57bd
Adith082 <adibhasan35@gmail.com>
2025-02-12 20:02:03 +0600

    variables and datatypes

--- Hello_world_Project/main.go
+++ Hello_world_Project/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Want to be free")
+	a := 30
+	fmt.Println(a)
+
+	var b bool = true
+	fmt.Println(b)
 }
+
+/*
+ golang language data types can be divided
+ into several types such as numeric
+ (integer, float, complex), String and boolean (bool)
+*/
